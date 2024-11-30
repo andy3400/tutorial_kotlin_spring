7f9cc4fd4d565fd8c954762227f666af2ea2715c
andy3400 <grayhat3400@gmail.com>
2024-11-30 22:58:03 +0900

    Feat : Add the Route

--- src/main/java/com/kotlin/demo/HttpControllers.kt
+++ src/main/java/com/kotlin/demo/HttpControllers.kt
@@ -1,23 +1,21 @@
 package com.kotlin.demo
 
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus.*
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/article")
-class HttpControllers(private val repository: ArticleRepository) {
+class ArticleController(private val repository: ArticleRepository) {
 
     @GetMapping("/")
     fun findAll() = repository.findAllByOrderByAddedAtDesc()
 
     @GetMapping("/{slug}")
     fun findOne(@PathVariable slug: String) =
-        repository.findBySlug(slug) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
-    }
+        repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
+
+}
 
 @RestController
 @RequestMapping("/api/user")
@@ -27,5 +25,5 @@ class UserController(private val repository: UserRepository) {
     fun findAll() = repository.findAll()
 
     @GetMapping("/{login}")
-    fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
+    fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
 }

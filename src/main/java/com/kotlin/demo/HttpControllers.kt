8553cd16073ed1eea72a4c817b62460d86140064
andy3400 <grayhat3400@gmail.com>
2024-11-30 23:02:46 +0900

    Feat : Expose API

--- src/main/java/com/kotlin/demo/HttpControllers.kt
+++ src/main/java/com/kotlin/demo/HttpControllers.kt
@@ -1,5 +1,6 @@
 package com.kotlin.demo
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -13,7 +14,7 @@ class ArticleController(private val repository: ArticleRepository) {
 
     @GetMapping("/{slug}")
     fun findOne(@PathVariable slug: String) =
-        repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
+        repository.findBySlug(slug) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
 
 }
 
@@ -25,5 +26,5 @@ class UserController(private val repository: UserRepository) {
     fun findAll() = repository.findAll()
 
     @GetMapping("/{login}")
-    fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
+    fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
 }

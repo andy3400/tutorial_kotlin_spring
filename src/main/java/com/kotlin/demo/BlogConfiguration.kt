27ea4c87444f6b0444038796e34442a431a6485d
andy3400 <grayhat3400@gmail.com>
2024-11-30 22:59:10 +0900

    Feat : Initial Data

--- src/main/java/com/kotlin/demo/BlogConfiguration.kt
+++ src/main/java/com/kotlin/demo/BlogConfiguration.kt
@@ -12,15 +12,16 @@ import org.springframework.context.annotation.Configuration
 class BlogConfiguration {
 
     @Bean
-    fun databaseInitializer(userRepository: UserRepository, articleRepository: ArticleRepository) = ApplicationRunner {
-        val johnDoe = userRepository.save(User("johndoe", "john", "Doe"))
+    fun databaseInitializer(userRepository: UserRepository,
+                            articleRepository: ArticleRepository) = ApplicationRunner {
+
+        val johnDoe = userRepository.save(User("johnDoe", "John", "Doe"))
         articleRepository.save(Article(
             title = "Lorem",
             headline = "Lorem",
             content = "dolor sit amet",
             author = johnDoe
         ))
-
         articleRepository.save(Article(
             title = "Ipsum",
             headline = "Ipsum",
